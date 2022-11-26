de6e1d42911d8e269bc0460c685d251b0a306ba4
Tirupathi Rao Bolla <75201848+tirupathirao2726@users.noreply.github.com>
2022-11-26 19:19:15 +0530

    Update settings.py

--- Assignment/Assignment/settings.py
+++ Assignment/Assignment/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-j5u$+9sw7-on*4ne1eq4!a-fuy9qwcp*ujwg81mx@rgerhgcpq"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
